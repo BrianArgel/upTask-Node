eaa8e501ef22f34ed30e115ba7e0c7c46a300b21
Brian <orobouro566@gmail.com>
2020-12-17 13:19:55 -0500

    actualizacion

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,16 +1,62 @@
-exports.poryectoshome = (req, res) => {
+const Proyectos = require('../models/Proyectos')
+const slug = require('slug')
+exports.poryectoshome = async(req, res) => {
+
+    const proyectos = await Proyectos.findAll();
     res.render('index', {
-        nombrePagina: 'Proyectos'
+        nombrePagina: 'Proyectos',
+        proyectos
     })
 }
 
-exports.formularioProyecto = (req, res) => {
+exports.formularioProyecto = async(req, res) => {
+    const proyectos = await Proyectos.findAll();
     res.render('nuevoProyecto', {
-        nombrePagina: 'Nuevo Proyecto'
+        nombrePagina: 'Nuevo Proyecto',
+        proyectos
     })
 }
-exports.nuevoProyecto = (req, res) => {
+exports.nuevoProyecto = async(req, res) => {
     //lo que el usuario escriba
+    const proyectos = await Proyectos.findAll();
 
-    console.log(req.body)
+
+    const { nombre } = req.body;
+    let errores = [];
+
+    if(!nombre) {
+        errores.push({'texto': 'Agrega un nombre al proyecto'})
+    }
+
+    //si hay errores
+    if(errores.length > 0) {
+        res.render('nuevoProyecto', {
+            nombrePagina: 'Nuevo Proyecto',
+            errores,
+            proyectos
+        
+        })
+    } else {
+        //conectar a la bas de datos si no hay errores
+     const proyecto = await Proyectos.create({ nombre })
+     res.redirect('/')
+       
+    }
+}  
+
+exports.proyectoPorUrl = async(req, res, next) => {
+    const proyectos = await Proyectos.findAll();
+    const proyecto = await Proyectos.findOne({
+        where: {
+            url: req.params.url
+        }
+    })
+
+    if(!proyecto) return next();
+
+    res.render('tareas', {
+        nombrePagina: 'Tareas del Proyecto',
+        proyecto,
+        proyectos
+    })
 }
--- index.js
+++ index.js
@@ -4,15 +4,30 @@ const routes = require('./routes/index')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+
+//crear la conexion a la base de datos
+const db = require('./config/db');
+// helpers con algunas funciones
+const helpers = require('./helpers')
+//importar el modelo 
+require('./models/Proyectos')
+db.sync()
+    .then(() => console.log('Conectado al servidor'))
+    .catch((e) => console.log(e))
 //donde cargar los archivos estaticos
 app.use(express.static('public'));
 //habilitar pug
 app.set('view engine', 'pug');
+//express validator
 
 //añadir la carpeta de las vistas
-app.set('views', path.join(__dirname, './views'));
-
-// Habilitar body parser datos del form
+app.set('views', path.join(__dirname, './views'))
+//pasar vardum a la aplicacion
+app.use((req, res, next) => {
+    res.locals.vardump = helpers.vardump;
+    next();
+})
+//body parser
 app.use(bodyParser.urlencoded({extended: true}))
 //creando app en expreess 
 app.use('/', routes())
