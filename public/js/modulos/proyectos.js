d71692128ad6354e5e9a468455fbccf69eeea946
Brian <orobouro566@gmail.com>
2020-12-19 11:24:20 -0500

    acciones boton

---
+++ public/js/modulos/proyectos.js
@@ -0,0 +1,39 @@
+import Swal from 'sweetalert2';
+import axios from 'axios'
+
+const btnEliminar = document.querySelector('#eliminar-proyecto');
+
+if(btnEliminar) {
+    btnEliminar.addEventListener("click", (e) => {
+        const urlProyecto = e.target.dataset.proyectoUrl;
+        //console.log(urlProyecto)
+        Swal.fire({
+            title: 'Deseas eliminar este proyecto?',
+            text: "un poryecto eliminado no se puede recuperar!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Borrar',
+            cancelButtonText: "No, cancelar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+                //enviar peticion a axios
+                
+              Swal.fire(
+                'Proyecto eliminado!',
+                'Proyecto se ha eliminado.',
+                'success'
+              );
+    
+              //redireccionar al inicio
+              setTimeout(() => {
+                window.location.href="/"
+              }, 3000)
+            }
+          })
+    })
+}
+
+export default btnEliminar;
